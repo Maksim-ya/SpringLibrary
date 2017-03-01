a41198cb5f9bdd0c3250eeabf107679f782cbdf0
Maksim-ya <Maksim-ya@ukr.net>
2017-03-01 19:55:52 +0200

    bean initialising

---
+++ src/main/java/maksimya/springlibrary/beans/TestBean.java
@@ -0,0 +1,19 @@
+package maksimya.springlibrary.beans;
+
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void test(){
+        System.out.println("sessionFactory = " + sessionFactory);
+    }
+
+}
+
